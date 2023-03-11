34bbd1350bddc0e4985d6092858483e77710754f
Christoph Rohrmoser <christoph.rohrmoser@objectbay.com>
2023-03-11 09:13:33 +0100

    Fix bug where jdtls does not work with Lombok

--- neovim/.config/nvim/ftplugin/java.lua
+++ neovim/.config/nvim/ftplugin/java.lua
@@ -79,7 +79,9 @@ local config = {
     -- See: https://github.com/eclipse/eclipse.jdt.ls#running-from-the-command-line
     cmd = {
         '/home/crohrmoser/tools/jdtls-eclipse/bin/jdtls',
-        '-javaagent:', '/home/crohrmoser/.local/share/eclipse/lombok.jar',
+        -- '-javaagent:', '/home/crohrmoser/.local/share/eclipse/lombok.jar',
+        '--jvm-arg=-javaagent:/home/crohrmoser/.local/share/eclipse/lombok.jar',
+        -- '-Xbootclasspath/a:', '/home/crohrmoser/.local/share/eclipse/lombok.jar',
         '-Declipse.application=org.eclipse.jdt.ls.core.id1',
         '-Dosgi.bundles.defaultStartLevel=4',
         '-Declipse.product=org.eclipse.jdt.ls.core.product',
@@ -89,15 +91,9 @@ local config = {
         '--add-modules=ALL-SYSTEM',
         '--add-opens', 'java.base/java.util=ALL-UNNAMED',
         '--add-opens', 'java.base/java.lang=ALL-UNNAMED',
-        -- ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^                                       ^^^^^^^^^^^^^^
-        -- Must point to the                                                     Change this to
-        -- eclipse.jdt.ls installation                                           the actual version
 
         '-jar', '/home/crohrmoser/tools/jdtls-eclipse/plugins/org.eclipse.equinox.launcher_1.6.400.v20210924-0641.jar',
         '-configuration', '/home/crohrmoser/tools/jdtls-eclipse/config_linux',
-        -- ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^        ^^^^^^
-        -- Must point to the                      Change to one of `linux`, `win` or `mac`
-        -- eclipse.jdt.ls installation            Depending on your system.
 
         -- Use the workspace_folder defined above to store data for this project
         '-data', workspace_folder,
