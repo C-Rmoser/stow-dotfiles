fe9b9f2e4e3042d4548665f230c6273ecec584ef
Christoph Rohrmoser <christoph.rohrmoser@objectbay.com>
2023-04-08 08:07:59 +0200

    Set up basic gitsigns mappings

--- neovim/.config/nvim/lua/chris/packer.lua
+++ neovim/.config/nvim/lua/chris/packer.lua
@@ -43,7 +43,21 @@ return require('packer').startup(function(use)
   use {
       'lewis6991/gitsigns.nvim',
       config = function()
-          require('gitsigns').setup()
+          require('gitsigns').setup {
+	      on_attach = function(bufnr)
+              local gs = package.loaded.gitsigns
+
+              local function map(mode, l, r, opts)
+                  opts = opts or {}
+                  opts.buffer = bufnr
+                  vim.keymap.set(mode, l, r, opts)
+              end
+
+              map('n', '<leader>tb', gs.toggle_current_line_blame)
+              map('n', '<leader>hb', function() gs.blame_line{full=true} end)
+              map('n', '<leader>hd', gs.diffthis)
+	      end
+          }
       end
   }
 
