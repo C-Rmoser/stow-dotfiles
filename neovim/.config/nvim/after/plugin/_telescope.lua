f567d2035d74cef15357019ae73a8f96e116eaf2
Christoph Rohrmoser <christoph.rohrmoser@objectbay.com>
2023-04-13 20:38:53 +0200

    Add additional telescope pickers

--- neovim/.config/nvim/after/plugin/_telescope.lua
+++ neovim/.config/nvim/after/plugin/_telescope.lua
@@ -5,10 +5,13 @@ vim.keymap.set('n', '<leader>fs', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fo', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 vim.keymap.set('n', '<leader>fc', builtin.commands, {})
+vim.keymap.set('n', '<leader>fj', builtin.jumplist, {})
+vim.keymap.set('n', '<leader>fk', builtin.keymaps, {})
 
 -- Lsp
 vim.keymap.set('n', '<leader>gr', builtin.lsp_references, {})
 vim.keymap.set('n', '<leader>gs', builtin.lsp_document_symbols, {})
+-- vim.keymap.set('n', '<leader>gs', builtin.lsp_document_symbols, {})
 -- vim.keymap.set('n', '<leader>gd', builtin.lsp_definitions, {})
 
 require("telescope").load_extension('harpoon')
