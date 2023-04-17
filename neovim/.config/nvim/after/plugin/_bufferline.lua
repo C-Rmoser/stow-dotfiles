592242de81a084febb8e7d727808ff5fd74837b7
Christoph Rohrmoser <christoph.rohrmoser@objectbay.com>
2023-04-17 19:09:30 +0200

    Add keymaps to be able to move buffers

--- neovim/.config/nvim/after/plugin/_bufferline.lua
+++ neovim/.config/nvim/after/plugin/_bufferline.lua
@@ -14,4 +14,6 @@ vim.keymap.set('n', '<leader>6', '<Cmd>BufferLineGoToBuffer 6<CR>', { noremap = 
 vim.keymap.set('n', '<leader>7', '<Cmd>BufferLineGoToBuffer 7<CR>', { noremap = true, silent = true})
 vim.keymap.set('n', '<leader>8', '<Cmd>BufferLineGoToBuffer 8<CR>', { noremap = true, silent = true})
 vim.keymap.set('n', '<leader>9', '<Cmd>BufferLineGoToBuffer 9<CR>', { noremap = true, silent = true})
-vim.keymap.set('n', '<leader>$', '<Cmd>BufferLineGoToBuffer -1<CR>', { noremap = true, silent = true})
+
+vim.keymap.set('n', '<leader><', '<Cmd>BufferLineMovePrev<CR>', { noremap = true, silent = true})
+vim.keymap.set('n', '<leader>>', '<Cmd>BufferLineMoveNext<CR>', { noremap = true, silent = true})
