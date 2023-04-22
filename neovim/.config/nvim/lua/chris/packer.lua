02a25ff52ec2413b5fc73f8f8d5866a4c1e5b0cd
Christoph Rohrmoser <christoph.rohrmoser@objectbay.com>
2023-04-22 21:00:08 +0200

    Remove toggleterm plugin

--- neovim/.config/nvim/lua/chris/packer.lua
+++ neovim/.config/nvim/lua/chris/packer.lua
@@ -204,16 +204,6 @@ return require('packer').startup(function(use)
         tag = 'nightly'                    -- optional, updated every week. (see issue #1193)
     }
 
-    use {
-        "akinsho/toggleterm.nvim", tag = '*',
-        config = function()
-            require("toggleterm").setup({
-                open_mapping = [[<c-\>]],
-                direction = 'float',
-            })
-        end
-    }
-
     use 'mfussenegger/nvim-dap'
 
     use {
