0cdb0f12adb6d1b4a9129a6fb99d162e5419ac95
Christoph Rohrmoser <christoph.rohrmoser@objectbay.com>
2023-05-18 16:13:37 +0200

    Adjust harpoon keymaps

--- neovim/.config/nvim/after/plugin/_harpoon.lua
+++ neovim/.config/nvim/after/plugin/_harpoon.lua
@@ -1,12 +1,10 @@
 local mark = require("harpoon.mark")
 local ui = require("harpoon.ui")
 
-local builtin = require('telescope.builtin')
 vim.keymap.set("n", "<leader>a", mark.add_file)
-vim.keymap.set('n', '<leader>fs', builtin.live_grep, {})
 vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
 
-vim.keymap.set("n", "<C-t>", function() ui.nav_file(1) end)
--- vim.keymap.set("n", "<C-h>", function() ui.nav_file(2) end)
-vim.keymap.set("n", "<C-n>", function() ui.nav_file(3) end)
-vim.keymap.set("n", "<C-p>", function() ui.nav_file(4) end)
+vim.keymap.set("n", "<leader>j", function() ui.nav_file(1) end)
+vim.keymap.set("n", "<leader>k", function() ui.nav_file(2) end)
+vim.keymap.set("n", "<leader>l", function() ui.nav_file(3) end)
+vim.keymap.set("n", "<leader>;", function() ui.nav_file(4) end)
