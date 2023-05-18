2eee892e76d20ad2f19f4569c92eef8ea6d331f0
Christoph Rohrmoser <christoph.rohrmoser@objectbay.com>
2023-05-18 16:16:35 +0200

    Remove treesitter context extension

--- neovim/.config/nvim/lua/chris/packer.lua
+++ neovim/.config/nvim/lua/chris/packer.lua
@@ -83,8 +83,6 @@ return require('packer').startup(function(use)
 
 	use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
 
-	use 'nvim-treesitter/nvim-treesitter-context'
-
 	use({
 		'ThePrimeagen/harpoon',
 		config = function()
