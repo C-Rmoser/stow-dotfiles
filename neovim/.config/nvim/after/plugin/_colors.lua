63a6c8a074801beb801c5a036a12a025c80703f0
Christoph Rohrmoser <christoph.rohrmoser@objectbay.com>
2023-05-24 15:18:35 +0200

    Change nvim colorscheme to everforest

--- neovim/.config/nvim/after/plugin/_colors.lua
+++ neovim/.config/nvim/after/plugin/_colors.lua
@@ -2,23 +2,25 @@ function ColorMyPencils(color)
 	color = color or "rose-pine"
 
 	vim.cmd [[
-        let g:everforest_background = 'hard'
+		let g:everforest_background = 'hard'
 	]]
 
 	vim.cmd.colorscheme(color)
-
-	-- vim.api.nvim_set_hl(0, "Normal", { bg = "None" })
+	vim.api.nvim_set_hl(0, "DiagnosticVirtualTextError", { link = "Error" })
+	vim.api.nvim_set_hl(0, "DiagnosticVirtualTextWarn", { link = "WarningMsg" })
+	vim.api.nvim_set_hl(0, "Normal", { bg = "None" })
+	vim.api.nvim_set_hl(0, "FloatBorder", { bg = "None" })
 	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "None" })
 end
 
 require('rose-pine').setup({
 	--- @usage 'auto'|'main'|'moon'|'dawn'
-	variant = 'moon',
+	variant = 'main',
 	--- @usage 'main'|'moon'|'dawn'
 	dark_variant = 'main',
 	bold_vert_split = false,
 	dim_nc_background = false,
-	disable_background = false,
+	disable_background = true,
 	disable_float_background = true,
 	disable_italics = false,
 	--- @usage string hex value or named color from rosepinetheme.com/palette
@@ -43,21 +45,11 @@ require('rose-pine').setup({
 			h5 = 'pine',
 			h6 = 'foam',
 		}
-		-- or set all headings at once
-		-- headings = 'subtle'
 	},
-	-- Change specific vim highlight groups
-	-- https://github.com/rose-pine/neovim/wiki/Recipes
 	highlight_groups = {
-		-- ColorColumn = { bg = 'rose' },
-
-		-- Blend colours against the "base" background
 		CursorLine = { bg = 'foam', blend = 10 },
 		StatusLine = { fg = 'love', bg = 'love', blend = 10 },
 	}
 })
 
--- Set colorscheme after options
--- ColorMyPencils()
--- ColorMyPencils('ayu-mirage')
 ColorMyPencils('everforest')
