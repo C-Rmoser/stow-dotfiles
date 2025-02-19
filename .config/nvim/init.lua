b598279ab7c36b900d60a159a13747252b126d0d
Christoph Rohrmoser <christoph.rohrmoser@objectbay.com>
2025-02-19 21:41:32 +0100

    Add luarocks paths to nvim config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,3 +1,8 @@
+-- Add LuaRocks path to package.path
+package.path = package.path .. ";/home/crohrmoser/.luarocks/share/lua/5.1/?.lua"
+package.path = package.path .. ";/home/crohrmoser/.luarocks/share/lua/5.1/?/init.lua"
+package.cpath = package.cpath .. ";/home/crohrmoser/.luarocks/lib/lua/5.1/?.so"
+
 require("config.maps")
 require("config.settings")
 require("config.lazy")
